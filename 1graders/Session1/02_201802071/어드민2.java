fa19ce8940cc667ad4833b6e67294bef831b6541
Kimsihoon123 <ksh0000n@naver.com>
2018-04-13 00:50:55 +0900

    2nd h.w.
    2nd h.w.

---
+++ 1graders/Session1/02_201802071/어드민2.java
@@ -0,0 +1,20 @@
+package ¾îµå¹Î2;
+
+public class ¾îµå¹Î2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0;
+		int b=1;
+		int c;
+		System.out.println(b);
+		for (int i = 1;i<10;i++) {
+			c=a+b;
+			a=b;
+			b=c;
+			System.out.println(c);
+		}
+
+	}
+
+}
