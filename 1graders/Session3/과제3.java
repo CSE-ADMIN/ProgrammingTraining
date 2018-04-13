62feaf92f5d12516c44b23e433c0258094585603
DESKTOP-2FISH38\seong <b5460881@naver.com>
2018-04-13 10:38:37 +0900

    과제3

---
+++ 1graders/Session3/과제3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class 과제3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("갯수를 입력하시오:");
+		int n = sc.nextInt();
+		int y = 0 ;
+		int i;
+		while(n>=y) {
+			
+	
+		for(i=1;y>=i;i++) {
+			System.out.print("*");
+		}
+		y++;
+		System.out.println();
+		}
+		
+	}
+		 }
